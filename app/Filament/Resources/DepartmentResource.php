fb3b1179de8b3b56662c42fe1f1d4d3a37cc0bb5
kevincisnero01 <kevingabriel18@gmail.com>
2023-08-27 22:22:41 -0400

    feat(resouce):  create department  resource

---
+++ app/Filament/Resources/DepartmentResource.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use Filament\Forms\Form;
+use App\Models\Department;
+use Filament\Tables\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\Section;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\DepartmentResource\Pages;
+use App\Filament\Resources\DepartmentResource\RelationManagers;
+
+class DepartmentResource extends Resource
+{
+    protected static ?string $model = Department::class;
+
+    protected static ?string $slug = 'departamentos';
+
+    protected static ?string $label = 'Departamentos';
+
+    protected static ?string $navigationLabel = 'Departamentos';
+
+    protected static ?int $navigationSort = 3;
+
+    protected static ?string $navigationGroup  = 'Ajustes';
+
+    protected static ?string $navigationIcon = 'heroicon-s-paper-clip';
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::getModel()::count();
+    }
+
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Section::make('Crear Departamento')
+                ->schema([
+                    TextInput::make('name')
+                        ->label('Nombre')
+                        ->required()
+                ])
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('id')
+                    ->label('ID')
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true)  
+                ,TextColumn::make('name')
+                    ->label('NOMBRE')
+                    ->sortable()
+                    ->toggleable()
+                    ->searchable()
+                ,TextColumn::make('created_at')
+                    ->label('FECHA DE CREACIÓN')
+                    ->date('d/m/Y')
+                    ->toggleable(isToggledHiddenByDefault: true)
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListDepartments::route('/'),
+            'create' => Pages\CreateDepartment::route('/create'),
+            'edit' => Pages\EditDepartment::route('/{record}/edit'),
+        ];
+    }    
+}
